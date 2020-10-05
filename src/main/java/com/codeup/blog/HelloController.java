cb2b38a787d02d9f77fd2954c850f43df8200d03
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-05 10:49:18 -0500

    created the project and completed the controllers lecture

---
+++ src/main/java/com/codeup/blog/HelloController.java
@@ -0,0 +1,21 @@
+package com.codeup.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+
+}
