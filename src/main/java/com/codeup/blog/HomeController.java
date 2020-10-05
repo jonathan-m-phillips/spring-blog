e812f4bcf46af1aa6f86356753dcbde0b99a4f69
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-05 11:34:47 -0500

    Math controller created

--- src/main/java/com/codeup/blog/HomeController.java
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -1,8 +1,10 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class HomeController {
 
     @GetMapping("/")
