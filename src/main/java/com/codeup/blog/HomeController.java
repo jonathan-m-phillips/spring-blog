cd81081202e7ac3fd892d1262541eaba62686cc2
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-05 11:27:21 -0500

    created home controller landing page

---
+++ src/main/java/com/codeup/blog/HomeController.java
@@ -0,0 +1,13 @@
+package com.codeup.blog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String home() {
+        return "This is the landing page!";
+    }
+}
