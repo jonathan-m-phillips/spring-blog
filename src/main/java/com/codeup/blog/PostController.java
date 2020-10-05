913f118c26be627360d792120b309f1937d5c35f
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-05 13:31:49 -0500

    forgot @Controller on Post

--- src/main/java/com/codeup/blog/PostController.java
+++ src/main/java/com/codeup/blog/PostController.java
@@ -1,10 +1,12 @@
 package com.codeup.blog;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class PostController {
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
