c10608e7f5906e5ac637f209bbc585d75ff3e863
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-05 12:08:52 -0500

    post controller created

---
+++ src/main/java/com/codeup/blog/PostController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+public class PostController {
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String posts() {
+        return "Posts index";
+    }
+
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String postsID(@PathVariable int id) {
+        return "This is the ID:" + id;
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String create() {
+        return "Creating post form";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String post() {
+        return "Posting form";
+    }
+
+
+}
