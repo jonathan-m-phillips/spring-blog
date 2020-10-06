e8ea9820d50701d9ddca1bf77bac86c5536c19b7
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-06 15:57:32 -0500

    almost complete

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -1,30 +1,29 @@
 package com.codeup.blog.controllers;
 
 import com.codeup.blog.models.Post;
+import com.codeup.blog.repository.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class PostController {
+    private final PostRepository postRepo;
+
+    public PostController(PostRepository postRepo){
+        this.postRepo = postRepo;
+    }
 
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
     public String showAllPosts(Model model) {
-        List<Post> postList = new ArrayList<>();
-        postList.add(new Post(0, "First Post", "This is the first Post"));
-        postList.add(new Post(0, "Second Post", "This is the second Post"));
-        model.addAttribute("posts", postList);
+        model.addAttribute("posts", postRepo.findAll());
         return "posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
     public String showOnePost(@PathVariable long id, Model model) {
-        Post post = new Post(0, "Single Post", "This is the body for the single post");
+        Post post = postRepo.getAdById(id);
         model.addAttribute("post", post);
         return "posts/show";
     }
