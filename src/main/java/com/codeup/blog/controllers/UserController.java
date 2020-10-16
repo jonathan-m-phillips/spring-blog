0318bb4bd05c5cebc86abf24795011c22036ff76
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2020-10-16 09:22:05 -0500

    created authentication controller and implemented copy consturctor in user model

--- src/main/java/com/codeup/blog/controllers/UserController.java
+++ src/main/java/com/codeup/blog/controllers/UserController.java
@@ -2,22 +2,43 @@ package com.codeup.blog.controllers;
 
 import com.codeup.blog.models.User;
 import com.codeup.blog.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
     private final UserRepository userDao;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userDao) {
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("user/{id}")
     public String profilePage(@PathVariable long id,Model model) {
         model.addAttribute("user", userDao.getOne(id));
         return "/users/profile";
+    }
+
+
+    @GetMapping("/sign-up")
+    public String signupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "/users/signup";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user) {
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
     }
 }
